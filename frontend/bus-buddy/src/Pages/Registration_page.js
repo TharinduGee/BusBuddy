5e5b45b65bc3f18103474c7abb809c31c13ccad3
Pabasara715 <pabasaravihanga715@gmail.com>
2023-12-05 02:01:48 +0530

    boostrap installed

---
+++ frontend/bus-buddy/src/Pages/Registration_page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Registration_page() {
+  return (
+    <div>
+      <div style={{ width: "100%", height: "100%", background: "#EFA818" }} />
+    </div>
+  );
+}
+
+export default Registration_page;
