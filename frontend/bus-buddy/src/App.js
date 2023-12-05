e6296a0cc481fb7aad2e393f686109e7203ab0e4
Pabasara715 <pabasaravihanga715@gmail.com>
2023-12-05 13:23:52 +0530

    app cs modified

--- frontend/bus-buddy/src/App.js
+++ frontend/bus-buddy/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
