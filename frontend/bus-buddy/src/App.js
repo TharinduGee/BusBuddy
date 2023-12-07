b88ebd5021a95c5a900551fc62653da65550997d
Lasitha Harshana <lasitha6646@gmail.com>
2023-12-08 02:38:27 +0530

    login page frontend partialy done

--- frontend/bus-buddy/src/App.js
+++ frontend/bus-buddy/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./Components/Header.jsx";
-
-import Landing_page from "./Pages/Landing_page.js";
-import Registration_page from "./Pages/Registration_page.js";
 
 function App() {
   return (
     <div className="App">
-      
+    
     </div>
   );
 }
