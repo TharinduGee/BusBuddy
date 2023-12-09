79aa93e4d90cb7ce772c42bc9d07b36cb14d6758
TharinduGee <tharindugeethanjan@gmail.com>
2023-12-09 22:59:20 +0530

    Dashboard responsiveness and flex components

---
+++ frontend/bus-buddy/src/Pages/User_Dashboard/UserManagement.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function User_Management(){
+     return(
+          <div>
+          <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+              User Management
+            </li>
+          </ol>
+        </nav>
+        <h1 class="h2">User Management</h1>
+        <p>
+          This is the user management interface of a simple admin interface 
+        </p> 
+          </div>
+     );
+}
+
+
+export default User_Management;
