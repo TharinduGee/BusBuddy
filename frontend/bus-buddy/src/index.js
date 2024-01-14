0388667dbad6e78f6e4b7aa0f41535e3428136e0
Lasitha Harshana <lasitha6646@gmail.com>
2024-01-14 11:46:53 +0530

    signup responsivedone

--- frontend/bus-buddy/src/index.js
+++ frontend/bus-buddy/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Boarding_Page from "./Pages/on_Boarding_Page/Boarding_Page";
+import Registration_Page from "./Pages/RegistraionPage/RegistrationPage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Boarding_Page />} />
+        <Route path="/signup" element={<Registration_Page />} />
       </Routes>
     </Router>
   </React.StrictMode>
