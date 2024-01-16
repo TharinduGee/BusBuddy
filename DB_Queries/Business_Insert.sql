fe998cfa53f3a7a87f6daa5ba4d9ddc6c98640b6
TharinduGee <tharindugeethanjan@gmail.com>
2024-01-16 23:11:04 +0530

    DDL script + authenticationService modification

---
+++ DB_Queries/Business_Insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO business (b_id ,address,b_name,registration_no )
+VALUES
+  (4,'Business1', 'business1@example.com', '123 Main St'),
+  (3,'Business2', 'business2@example.com', '456 Broad St');
+  -- Add more rows as needed
+
