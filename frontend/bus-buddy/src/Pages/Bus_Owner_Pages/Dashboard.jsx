8279626c84b94ef4e07bc5faed8c6d2a1b67a2fb
Pabasara715 <pabasaravihanga715@gmail.com>
2024-03-06 13:05:42 +0530

    rechart integration

--- frontend/bus-buddy/src/Pages/Bus_Owner_Pages/Dashboard.jsx
+++ frontend/bus-buddy/src/Pages/Bus_Owner_Pages/Dashboard.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Navbar from "../../Components/OwnerPageComponents/Sidebar";
 import Sidebar from "../../Components/OwnerPageComponents/Sidebar";
+import Chart from "../../Components/chart/Chart";
 
 function Dashboard() {
+  const data = [
+    { name: "Monday", Total: 1200, Expenses: 600 },
+    { name: "Tuesday", Total: 2100, Expenses: 1000 },
+    { name: "Wednsday", Total: 800, Expenses: 400 },
+    { name: "Thursday", Total: 1600, Expenses: 900 },
+    { name: "Friday", Total: 900, Expenses: 300 },
+    { name: "Saturday", Total: 1700, Expenses: 1200 },
+    { name: "Sunday", Total: 1100, Expenses: 400 },
+  ];
+
   return (
     <Sidebar>
-      <div className="d-flex flex-row justify-content-start">
-        <h1>Dashboaddddddrd</h1>
+      <div className="d-flex flex-column justify-content-start">
+        <h1>Dashboad</h1>
+        <div style={{ width: "80%" }}>
+          <Chart title="Income" aspect={2 / 1} data={data} />
+        </div>
       </div>
     </Sidebar>
   );
