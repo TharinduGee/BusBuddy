80dad18de7fd8d4f317092609cfd5d70d11c535c
Tharindu <tharindugeethanjan@gmail.com>
2024-03-14 15:03:46 +0530

    minor bug fixes 😁😁😁

---
+++ backend/BusBuddy/src/main/java/com/example/BusBuddy/dto/Document/DocumentResponse.java
@@ -0,0 +1,20 @@
+package com.example.BusBuddy.dto.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentResponse {
+    private Long docId;
+    private String docName;
+    private LocalDate uploadDate;
+    private
+    private Long RefId;
+}
