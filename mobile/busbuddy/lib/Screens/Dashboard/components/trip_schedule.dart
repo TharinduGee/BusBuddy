89585d1da41a447330c3ff9c35a18cd99c82c30a
Lasitha Harshana <lasitha6646@gmail.com>
2024-04-02 01:18:24 +0530

    add elevation to card

--- mobile/busbuddy/lib/Screens/Dashboard/components/trip_schedule.dart
+++ mobile/busbuddy/lib/Screens/Dashboard/components/trip_schedule.dart
@@ -142,86 +142,94 @@ class _TripScheduleState extends State<TripSchedule> {
               ),
             ),
             Container(
-              margin: const EdgeInsets.all(8),
-              color: Colors.amber,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          color: Colors.black38,
-                          child: const Text(
-                            "Start Destination ",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          color: Colors.black38,
-                          child: const Text(
-                            ": Galle at 10:24",
-                            style: TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          color: Colors.black38,
-                          child: const Text(
-                            "End Destination ",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          color: Colors.black38,
-                          child: const Text(
-                            ": Badulla at 17:45",
-                            style: TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                color: Colors.amber,
+                shadowColor: Colors.black,
+                elevation: 20.0,
+                borderRadius: BorderRadius.circular(16),
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Container(
-                          color: Colors.black38,
-                          child: const Text(
-                            "Conducter ID : 3456897ID",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              color: Colors.black12,
+                              child: const Text(
+                                "Start Destination ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
                             ),
-                          ),
+                            Container(
+                              color: Colors.black12,
+                              child: const Text(
+                                ": Galle at 10:24",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              color: Colors.black12,
+                              child: const Text(
+                                "End Destination ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              color: Colors.black12,
+                              child: const Text(
+                                ": Badulla at 17:45",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              color: Colors.black12,
+                              child: const Text(
+                                "Conducter ID : 3456897ID",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
