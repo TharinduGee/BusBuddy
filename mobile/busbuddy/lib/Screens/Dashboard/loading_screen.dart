ad325191e61ba30e3d55c7e79c65e9b9680e5a59
Lasitha Harshana <lasitha6646@gmail.com>
2024-04-16 22:24:52 +0530

    add lottie dependence

---
+++ mobile/busbuddy/lib/Screens/Dashboard/loading_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Loadingscreen extends StatelessWidget {
+  const Loadingscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
